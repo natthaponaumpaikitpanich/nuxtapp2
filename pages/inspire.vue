103197b0555c03728f5b3cf7e8c4099c5369f629
Your Name <natthaponaumpaikitpanich1@gmail.com>
2025-11-10 12:11:01 +0700

    first commit

---
+++ pages/inspire.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-row>
+    <v-col class="text-center">
+      <img src="/v.png" alt="Vuetify.js" class="mb-5" />
+      <blockquote class="blockquote">
+       " First, solve the problem. Then, write the code"
+        <footer>
+          <small>
+            <em>ทดลองพิมพ์ไทย</em>
+          </small>
+        </footer>
+      </blockquote>
+    </v-col>
+  </v-row>
+</template>
+<template>
+  <div>
+    <center>
+  <v-btn>
+    click
+  </v-btn>
+</center> 
+</div>
+   
+</template>
+<script>
+export default {
+  name: 'InspirePage',
+}
+</script>
